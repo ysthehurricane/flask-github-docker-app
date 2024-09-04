cbf5b45afa1ca2f5c1e4962a3eb2e1c2426cc01c
Yash Patel <patelyash2504@gmail.com>
2024-09-04 18:12:14 +0530

    Add New route

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask with Docker!"
 
+
+@app.route('/index')
+def index():
+    return "Hello, I am Yash!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
